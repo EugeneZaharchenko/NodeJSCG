139777bb5ad345640b67c2f0580e1628654be1c6
Eugene Zakharchenko <eugenezaharchenko@gmail.com>
2024-12-23 23:19:50 +0200

    using routes file

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const routes = require("./routes");
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.setHeader("Custom-Header", "my-header");
-  res.send("Hello from Node.js Docker container! Try changing HO-JHO ))).");
-});
+// Apply routes
+app.use("/", routes.router);
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server running at http://localhost:${port}`);
+  console.log(routes.someText);
 });
---
+++ routes.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const fs = require("fs");
+const router = express.Router();
+
+// Home route
+router.get("/", (req, res) => {
+  res.setHeader("Custom-Header", "my-header");
+  res.send("Hello from Node.js Docker container! Try changing HO-JHO ))).");
+});
+
+// Synchronous message route
+router.get("/mess", (req, res) => {
+  fs.writeFileSync("message.txt", "Try changing it, HO-HO ))).");
+  res.redirect("/");
+});
+
+// Asynchronous message route
+router.get("/messAsync", (req, res) => {
+  fs.writeFile("messageAsync.txt", "HO-HO ))).", (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error writing to file");
+      return;
+    }
+  });
+  res.redirect("/");
+});
+
+module.exports = {
+  router: router,
+  someText: "Hello from routes.js",
+};
