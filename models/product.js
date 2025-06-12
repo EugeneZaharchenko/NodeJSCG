aaa2c7a9b6633ac220c640cfe20a0e8eb49ce324
Y.Zakharchenko <Y.Zakharchenko@steel.mil.ua>
2025-06-12 16:42:55 +0300

    model refactored to store in file

--- models/product.js
+++ models/product.js
@@ -1,15 +1,47 @@
-const products = [];
+import { readFile, writeFile } from "fs";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
 
-module.exports = class Product {
-    constructor(t) {
-        this.title = t;
-    }
+// Отримаємо шлях до поточного файлу
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-    save() {
-        products.push(this);
-    }
+export default class Product {
+  constructor(t) {
+    this.title = t;
+  }
 
-    static fetchAll() {
-        return products;
-    }
+  save() {
+    const p = join(__dirname, "..", "data", "products.json");
+    readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        try {
+          products = JSON.parse(fileContent);
+        } catch (parseErr) {
+          console.log("Помилка парсингу JSON:", parseErr);
+        }
+      }
+      products.push(this);
+      writeFile(p, JSON.stringify(products), (err) => {
+        if (err) console.log("Помилка запису:", err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    const p = join(__dirname, "..", "data", "products.json");
+    readFile(p, (err, fileContent) => {
+      if (err) {
+        return cb([]);
+      }
+      try {
+        const data = JSON.parse(fileContent);
+        cb(data);
+      } catch (parseErr) {
+        console.log("Помилка парсингу JSON:", parseErr);
+        cb([]);
+      }
+    });
+  }
 }
